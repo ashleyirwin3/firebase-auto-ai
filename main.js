2d666087f814fee21baeb6d9a9ec467c5cc95586
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-12 11:34:10 -0600

    added initialize app function for user email and password

--- main.js
+++ main.js
@@ -5,4 +5,28 @@ $('form').submit((e) => {
   $('.login-page').addClass('hidden')
   $('.main-page').removeClass('hidden')
   e.preventDefault()
+
+})
+  firebase.initializeApp({
+    apiKey: "AIzaSyD2NMaJ5Is0KaIFXPuR50bo7qB320cmWMM",
+    authDomain: "fir-auto-ai.firebaseapp.com",
+    databaseURL: "https://fir-auto-ai.firebaseio.com",
+    storageBucket: "fir-auto-ai.appspot.com",
+    messagingSenderId: "781941044326"
+  });
+
+$('form').submit((e) => {
+  var email = $('input[type="email"]').val()
+  var password =  $('input[type="password"]').val()
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+        $('.main-page h1').text(`Welcome ${email}`)
+        $('.login-page').addClass('hidden')
+        $('.main-page').removeClass('hidden')
+  })
+
+  e.preventDefault()
 })
